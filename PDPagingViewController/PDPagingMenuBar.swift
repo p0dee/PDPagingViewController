5ae05821abb6ba22c88b87b7a042962733d9c954
p0dee <t.takeshi.uc@gmail.com>
2017-02-25 17:17:53 +0900

    メニューバークラスの追加

---
+++ PDPagingViewController/PDPagingMenuBar.swift
@@ -0,0 +1,13 @@
+//
+//  PDPagingMenuBar.swift
+//  PDPagingViewController
+//
+//  Created by Takeshi Tanaka on 2017/02/25.
+//  Copyright © 2017 p0dee. All rights reserved.
+//
+
+import UIKit
+
+class PDPagingMenuBar: UIView {
+    
+}
