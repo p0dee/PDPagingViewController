38d0f588a00df68e272b1d46c182d33450e16fdd
p0dee <t.takeshi.uc@gmail.com>
2017-02-26 22:29:28 +0900

    コントローラ 初期化仮実装を追加
    ベースとなるスクロールビュー上に view controller を配置する処理を簡単に実装した。

--- PDPagingViewController/PDPagingViewController.swift
+++ PDPagingViewController/PDPagingViewController.swift
@@ -12,6 +12,8 @@ class PDPagingViewController: UIViewController {
     
     var viewControllers: [UIViewController]
     
+    lazy var scrollView = UIScrollView()
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         viewControllers = []
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -23,6 +25,26 @@ class PDPagingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpViews()
+        setUpViewControllers()
+    }
+    
+    func setUpViews() {
+        scrollView.frame = view.bounds
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(scrollView)
+        var contentSize = view.bounds.size
+        contentSize.width *= CGFloat(viewControllers.count)
+        scrollView.contentSize = contentSize
+    }
+    
+    func setUpViewControllers() {
+        viewControllers.forEach {
+            self.addChildViewController($0)
+            view.addSubview($0.view)
+            //TODO: 以下実装追加
+            // https://developer.apple.com/library/content/featuredarticles/ViewControllerPGforiPhoneOS/ImplementingaContainerViewController.html
+        }
     }
     
 }
