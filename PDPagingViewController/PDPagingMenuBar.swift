c0b9d9f636cd47254c1af2214f9fa9f6bf71dd88
p0dee <t.takeshi.uc@gmail.com>
2017-02-26 22:26:45 +0900

    メニューバークラス 初期化実装を追加
    - 必要なビューとしてスクロールビューの設定を実装した

--- PDPagingViewController/PDPagingMenuBar.swift
+++ PDPagingViewController/PDPagingMenuBar.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class PDPagingMenuBar: UIView {
     
+    private lazy var scrollView = UIScrollView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(titles: [String]) {
+        self.init()
+        setUpViews()
+    }
+    
+    private func setUpViews() {
+        scrollView.frame = bounds
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(scrollView)
+    }
+    
 }
